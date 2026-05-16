55c54f134baf7c4012f98810fb58690f5d7c8455
Sam Wilson <sam@samwilson.id.au>
2026-05-16 17:10:28 +0100

    Set user-agent for all API calls
    Centralise the construction of a user-agent string, and use it
    in all places it's needed.
    
    Also fix a typo and a missing l10n message.

---
+++ src/OAuthOwnerConsumer.php
@@ -0,0 +1,23 @@
+<?php
+// phpcs:disable
+
+namespace Piwigo\Plugin\WikimediaCommons;
+
+use Addwiki\Mediawiki\Api\Client\Auth\OAuthOwnerConsumer as AddWikiOAuthOwnerConsumer;
+
+/**
+ * This class is a temporary workaround in order to set the user agent.
+ */
+class OAuthOwnerConsumer extends AddWikiOAuthOwnerConsumer
+{
+
+  private ?string $identifierForUserAgent = null;
+
+  public function setIdentifierForUserAgent($identifier): void {
+    $this->identifierForUserAgent = $identifier;
+  }
+
+  public function identifierForUserAgent(): ?string {
+    return $this->identifierForUserAgent ?? parent::identifierForUserAgent();
+  }
+}
